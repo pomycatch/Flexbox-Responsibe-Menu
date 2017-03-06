b69d009cba7a16c421642ea0216cd801c11fff0a
pomycatch <pomykacz.szymon7@gmail.com>
2017-03-06 21:21:12 +0100

    small fixes

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 $(function(){
 
-
-	
-
-
-
 		$('button').click(function(){
 			$(".main-menu").toggle();		
 		});
 
-		$(".main-menu ul li a").each(function() {
+		$(".first-level a").not(".second-level a").each(function() {
 			if ($(this).next().length > 0) {
 			//	$(this).parent().addClass("parent");
 				$(this).append("<i class='fa fa-caret-down'></i>");
 			}
 		});
+
+		$(".second-level li a").each(function() {
+			if ($(this).next().length > 0) {
+			//	$(this).parent().addClass("parent");
+				$(this).append("<i class='fa fa-caret-right'></i>");
+			}
+		});
+
 
 		$('.main-menu ul li a, .second-level li a').click(function(e){
 			var a_href = $(this).attr('href');
@@ -25,8 +28,4 @@ $(function(){
 		});
 
 	
-
-	
-
-
 });
